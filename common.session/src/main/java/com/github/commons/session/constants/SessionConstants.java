d4d4c759d8ee7da49cb847f5cd0a8e8eac6c6eaa
xiaofengzhouxf <xiaofeng.zhouxf83@gmail.com>
2015-07-28 15:50:37 +0800

    seq

---
+++ common.session/src/main/java/com/github/commons/session/constants/SessionConstants.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2015 linglingqi Group Holding Ltd.
+ *
+ * 
+ */
+package com.github.commons.session.constants;
+
+/**
+ * SessionConstants.java
+ *
+ * @author zhouxiaofeng 7/15/15
+ */
+public class SessionConstants {
+}
